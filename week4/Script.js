175c5c0c296b3f813455f1e4492c4a83c481a747
Refiando535180098 <70618834+Refiando535180098@users.noreply.github.com>
2020-09-23 22:58:41 +0700

    Week4
    Tugas pertemuan ke 4 JavaScript

---
+++ week4/Script.js
@@ -0,0 +1,6 @@
+function changeLine1Style(){
+    document.getElementById(cLine1).className = "Makered";
+}
+function ChangeStyle(id, classname){
+    document.getElementById(id).className = classname;
+}
